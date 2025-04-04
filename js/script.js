dbbfecfdc86dd4cdfbf97b32487bab10906ce0cf
Khaled Amoudi <67203109+khaled-amoudi@users.noreply.github.com>
2025-04-04 02:57:22 +0200

    add static pages and finish the design | still the first tab of card details

--- js/script.js
+++ js/script.js
@@ -77,6 +77,19 @@ const swiper2 = new Swiper(".swiper2", {
     direction: "horizontal",
     loop: true,
     grabCursor: true,
+
+    // effect: "coverflow",
+    // grabCursor: true,
+    // centeredSlides: true,
+    // coverflowEffect: {
+    //     rotate: 50,
+    //     stretch: 0,
+    //     depth: 100,
+    //     modifier: 1,
+    //     slideShadows: true,
+    // },
+
+    
     autoplay: {
         delay: 3000,
     },
